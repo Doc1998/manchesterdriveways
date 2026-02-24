62c4f036d2c19ddee297a32c8c21cfaefd815f89
Doc1998 <docmp98@gmail.com>
2026-02-24 15:14:56 +0000

    updates

---
+++ src/_data/client.js
@@ -0,0 +1,21 @@
+module.exports = {
+    "name": "Manchester Driveways LTD",
+    "email": "manchesterdrivewaysltd1@gmail.com",
+    "phoneForTel": "+447585551012",
+    "phoneFormatted": "+44 7585 551012",
+    "address": {
+        "lineOne": "",
+        "lineTwo": "",
+        "city": "Greater Manchester",
+        "state": "",
+        "zip": "",
+        "country": "England",
+        "mapLink": "https://share.google/M2KBpWdsi0e50Z4No"
+    },
+    "socials": {
+        "facebook": "https://www.facebook.com/Manchesterdrivewaysltd",
+        "instagram": "https://www.instagram.com/",
+    },
+    //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
+    "domain": "https://www.manchesterdrivewaysltd.co.uk"
+};
